5dc216f4a7f0233f19229f4bde32c691b506fd93
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-03-12 10:04:35 +0100

    updated square row and column definition

--- src/components/chessboard/index.tsx
+++ src/components/chessboard/index.tsx
@@ -10,10 +10,10 @@ function ChessBoard() {
     <div className="order-1 w-full md:w-[33rem] md:h-[33rem] h-[60vh] lg:w-[40rem] lg:w-[40rem] bg-white">
       <div className="w-full h-full grid grid-cols-8 grid-rows-{8}">
         {data.map((squareRow: Array<ChessSquare>) => {
-          return squareRow.map((chessSquare: ChessSquare) => (
+          return squareRow.map((squareColumn: ChessSquare) => (
             <Square
-              square={chessSquare}
-              key={`${chessSquare.coordinates[0]}-${chessSquare.coordinates[1]}`}
+              square={squareColumn}
+              key={`${squareColumn.coordinates[0]}-${squareColumn.coordinates[1]}`}
             />
           ));
         })}
